bca66690605007c65e56118ddab3cebe499ff05e
msrissaxox <marissa.bianca.lamothe@gmail.com>
2024-12-10 16:51:02 -0500

    third commit

--- index.js
+++ index.js
@@ -10,14 +10,7 @@ import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 import bodyParser from "body-parser";
 
-//get the password and put it in a variable
-
-
-//remove this and use body parser to get the data
-// const password = document.getElementById("password");
-
 app.use(bodyParser.urlencoded({ extended: true }))
-
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
@@ -29,11 +22,7 @@ app.post("/check", (req, res) => {
     res.sendFile(__dirname + "/public/secret.html");
 } else {
     res.sendFile(__dirname + "/public/index.html");
-
-}
-    
-}
-)
+}});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
